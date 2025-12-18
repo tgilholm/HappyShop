702b94eb80dcc07ab744ce97017fc82508c6c919
tom <tgilholm@gmail.com>
2025-12-18 11:18:58 +0000

    Improved SetDatabase method with separation of concerns and improved methods

--- src/main/java/ci553/happyshop/systemSetup/SetDatabase.java
+++ src/main/java/ci553/happyshop/systemSetup/SetDatabase.java
@@ -1,7 +1,10 @@
 package ci553.happyshop.systemSetup;
 
+import ci553.happyshop.data.database.DatabaseConnection;
+import ci553.happyshop.data.database.DatabaseException;
 import ci553.happyshop.storageAccess.DatabaseRWFactory;
 import ci553.happyshop.utility.StorageLocation;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.file.*;
@@ -26,20 +29,19 @@ import java.sql.*;
  */
 
 
-
 public class SetDatabase
 {
     // Create the database if it doesn't exist
     private static final String dbURL = DatabaseRWFactory.dbURL + ";create=true";
 
-    private static Path imageWorkingFolderPath = StorageLocation.imageFolderPath;
-    private static Path imageBackupFolderPath = StorageLocation.imageResetFolderPath;
+    // Get a dbConnection
+    private static final DatabaseConnection dbConnection = new DatabaseConnection(dbURL);
 
-    // Array of table names
-    private String[] tables =
-            {"ProductTable", "LoginTable", "BasketTable", "CategoryTable"};
+    private static final Path imageWorkingFolderPath = StorageLocation.imageFolderPath;
+    private static final Path imageBackupFolderPath = StorageLocation.imageResetFolderPath;
 
-    // Table Creation SQL
+
+    // Table Schema Definition
     private static final String[] CREATE_TABLES = {
 
             // Each table has a BIGINT primary key that is automatically generated
@@ -47,7 +49,7 @@ public class SetDatabase
             CREATE TABLE CategoryTable (
                 id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
                 name VARCHAR(50) NOT NULL UNIQUE,
-                description VARCHAR(255),
+                description VARCHAR(255)
             )
             """,
 
@@ -100,7 +102,7 @@ public class SetDatabase
     };
 
     // Default products with categoryID added
-    // IDs do not need to be specified because they are automatically generated
+    // Product IDs do not need to be specified because they are automatically generated
     private static final String[] DEFAULT_PRODUCTS = new String[]{
             "INSERT INTO ProductTable(name, imageName, unitPrice, stockQuantity, categoryID) VALUES('40 inch TV', '0001.jpg', 269.00, 100, 1)",
             "INSERT INTO ProductTable(name, imageName, unitPrice, stockQuantity, categoryID) VALUES('DAB Radio', '0002.jpg', 29.99, 100, 2)",
@@ -123,131 +125,122 @@ public class SetDatabase
 
     public static void main(String[] args) throws SQLException, IOException
     {
-        SetDatabase setDB = new SetDatabase();
-        setDB.clearTables();
-        setDB.initializeTable();
-        setDB.queryTableAfterInitilization();
+        clearTables();          // Delete tables
+        createTables();         // Add tables
+        initializeTables();     // Populate tables
         deleteFilesInFolder(imageWorkingFolderPath);
         copyFolderContents(imageBackupFolderPath, imageWorkingFolderPath);
-
     }
 
-    // Reset tables
-    private void clearTables() throws SQLException
+    /**
+     * Calls <code>DROP TABLE</code> on all tables and handles non-existent tables
+     */
+    private static void clearTables()
     {
-        try (Connection con = DriverManager.getConnection(dbURL); Statement statement = con.createStatement())
+        System.out.println("Use better logging than this!");
+        System.out.println("Dropping all tables");
+
+        // Drop tables in reverse to avoid foreign keys being null
+        String[] tables = {"BasketTable", "ProductTable", "LoginTable", "CategoryTable"};
+
+        try (Connection connection = dbConnection.getConnection();
+             Statement statement = connection.createStatement())
         {
-            System.out.println("Database happyShopDB is connected successfully!");
             for (String table : tables)
             {
                 try
                 {
-                    // Try to drop table directly
                     statement.executeUpdate("DROP TABLE " + table.toUpperCase());
                     System.out.println("Dropped table: " + table);
-                } catch (SQLException e)
+                } catch (SQLException e)    // throws SQLException if the table doesn't exist
                 {
                     if ("42Y55".equals(e.getSQLState()))
                     { // 42Y55 = Table does not exist
-                        System.out.println("Table " + table + " does not exist. Skipping...");
+                        System.out.println(table + " does not exist");
                     }
                 }
             }
-        } finally
+        } catch (SQLException e)
         {
+            throw new DatabaseException("Failed to clear tables", e);
         }
     }
 
-    // Creates tables and initializes them with data
-    private void initializeTable() throws SQLException
+    /**
+     * Creates new database tables from the schema
+     */
+    private static void createTables()
     {
-        // Run the SQL statements
-        try (Connection connection = DriverManager.getConnection(dbURL))
-        {
-            System.out.println("Database happyShopDB is created successfully!");
-            connection.setAutoCommit(false); // Disable auto-commit for the batch
+        System.out.println("Creating tables");
 
+        try (Connection connection = dbConnection.getConnection();
+             Statement statement = connection.createStatement())
+        {
+            for (String createTable : CREATE_TABLES)
+            {
+                statement.executeUpdate(createTable);
+            }
+
+        } catch (SQLException e)
+        {
+            throw new DatabaseException("Failed to create new tables", e);
+        }
+    }
+
+
+    /**
+     * Initializes the tables with the pre-set data
+     */
+    private static void initializeTables()
+    {
+        System.out.println("Populating tables");
+
+        try (Connection connection = dbConnection.getConnection())
+        {
             try (Statement statement = connection.createStatement())
             {
-                // First, create the table (DDL) - Execute this one separately from DML
-                statement.executeUpdate(iniTableSQL[0]); // Execute Create Table SQL
+                connection.setAutoCommit(false); // Disable auto-commit for the batch
 
-                // Prepare and execute the insert operations (DML)
-                for (int i = 1; i < iniTableSQL.length; i++)
+                // Insert categories first, referenced by Products
+                for (String sql : DEFAULT_CATEGORIES)
                 {
-                    statement.addBatch(iniTableSQL[i]); // Add insert queries to batch
+                    statement.addBatch(sql);    // Add to the statement
                 }
+                statement.executeBatch();       // Execute the statements
+                statement.clearBatch();         // Reset the statement
 
-                // Execute all the insert statements in the batch
+                // Insert users
+                for (String sql : DEFAULT_USERS)
+                {
+                    statement.addBatch(sql);
+                }
                 statement.executeBatch();
+                statement.clearBatch();
+
+                // Insert products
+                for (String sql : DEFAULT_PRODUCTS)
+                {
+                    statement.addBatch(sql);
+                }
+                statement.executeBatch();
+                statement.clearBatch();
                 connection.commit(); // Commit the transaction if everything was successful
 
                 System.out.println("Table and data initialized successfully.");
 
             } catch (SQLException e)
             {
-                connection.rollback(); // Rollback the transaction in case of an error
-                System.err.println("Transaction rolled back due to an error!");
-                e.printStackTrace();
+                connection.rollback(); // Roll back the connection if there was an error
+                throw new DatabaseException("Failed to add transactions, rolling back", e);
             }
-        } finally
+        } catch (SQLException e)
         {
-        }
-    }
-
-
-    // Output all the data in the tables after initialising them
-    private void queryTableAfterInitilization() throws SQLException
-    {
-        // Query ProductTable
-        String sqlQuery = "SELECT * FROM ProductTable";
-
-        System.out.println("------------- ProductTable -----------------");
-        String title = String.format("%-12s %-20s %-10s %-10s %s", "productID", "description", "unitPrice", "inStock",
-                "image");
-        System.out.println(title); // Print formatted output
-
-
-        try (Connection connection = DriverManager.getConnection(dbURL); Statement stat = connection.createStatement())
-        {
-            ResultSet resultSet = stat.executeQuery(sqlQuery);
-            while (resultSet.next())
-            {
-                String productID = resultSet.getString("productID");
-                String description = resultSet.getString("description");
-                double unitPrice = resultSet.getDouble("unitPrice");
-                String image = resultSet.getString("image");
-                int inStock = resultSet.getInt("inStock");
-                String record = String.format("%-12s %-20s %-10.2f %-10d %s", productID, description, unitPrice,
-                        inStock, image);
-                System.out.println(record); // Print formatted output
-            }
-
-
-            sqlQuery = "SELECT * From LoginTable";
-            System.out.println("------------- LoginTable -----------------");
-
-            try
-            {
-                resultSet = stat.executeQuery(sqlQuery);
-                while (resultSet.next())
-                {
-                    String userID = resultSet.getString("userID");
-                    String username = resultSet.getString("username");
-                    String password = resultSet.getString("password");
-                    String record = String.format("%s %20s %20s", userID, username,
-                            password);
-                    System.out.println(record); // Print formatted output
-                }
-            } finally
-            {
-
-            }
+            throw new DatabaseException("Failed to add transactions", e);
         }
     }
 
     // Recursively deletes all files in a folder
-    public static void deleteFilesInFolder(Path folder) throws IOException
+    public static void deleteFilesInFolder(Path folder)
     {
         if (Files.exists(folder))
         {
@@ -257,16 +250,17 @@ public class SetDatabase
                 Files.walkFileTree(folder, new SimpleFileVisitor<>()
                 {
                     @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException
+                    public @NotNull FileVisitResult visitFile(@NotNull Path file,
+                            @NotNull BasicFileAttributes attrs) throws IOException
                     {
                         Files.delete(file); // delete individual files
                         return FileVisitResult.CONTINUE;
                     }
                 });
                 System.out.println("Deleted files in folder: " + folder);
-            } finally
+            } catch (IOException e)
             {
-
+                throw new DatabaseException("Failed to delete files", e);
             }
         } else
         {
@@ -274,52 +268,31 @@ public class SetDatabase
         }
     }
 
+
     /**
-     * The method Files.walkFileTree(Path, FileVisitor) traverses (or "walks
-     * through") a directory and all of its subdirectories. It accepts two
-     * arguments: 1. directory (Path or folder) path from which the traversal begins
-     * (the starting point of the walk). 2. A FileVisitor object that defines the
-     * actions to be performed when a file or directory is visited. The visitor is
-     * an instance of the FileVisitor interface, which provides methods for handling
-     * different events during the traversal.
-     * <p>
-     * Here, we use an anonymous class to create the second argument - the instance
-     * (object) – An anonymous class allows you to extend a superclass (or implement
-     * an interface) and instantiate it in a single, concise step, without needing
-     * to define a separate named class. It combines both class extension and object
-     * creation into one operation, typically used when you need a one-off
-     * implementation of a class or interface. (Note: the object is the anonymous
-     * class's)
-     * <p>
-     * We did not use Files.walkFileTree(folder, new FileVisitor<>()) because
-     * FileVisitor is an interface, and we would need to implement all of its
-     * methods ourselves. Instead, we use Files.walkFileTree(folder, new
-     * SimpleFileVisitor<>()) because: - SimpleFileVisitor<> is an abstract class
-     * that implements the FileVisitor interface with default method
-     * implementations. - We only need to override the methods (visitFile,
-     * postVisitDirectory) that we're interested in, which simplifies our code.
+     * Copies all the files from the <code>source</code> (backup) directory to the <code>destination</code> (working) directory
+     * @param source the <code>Path</code> to the directory to copy from
+     * @param destination the <code>Path</code> to the directory to copy to
      */
-
-    // Copies all files from source folder to destination folder
-    public static void copyFolderContents(Path source, Path destination) throws IOException
+    public static void copyFolderContents(Path source, Path destination)
     {
-
+        // If the source folder does not exist, throw an error
         if (!Files.exists(source))
         {
-            throw new IOException("Source folder does not exist: " + source);
+            throw new DatabaseException("Source folder does not exist: " + source);
         }
-
-        // Create destination folder if it doesn't exist
-        if (!Files.exists(destination))
-        {
-            Files.createDirectories(destination);
-        }
-
-        // Copy files from source folder to destination folder
+        // Copy files from the source folder to destination folder
         // Files.newDirectoryStream(source): list all entries (files and folders)
-        // directly in the source directory
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(source))
         {
+
+            // Create the destination folder if it doesn't exist
+            if (!Files.exists(destination))
+            {
+                Files.createDirectories(destination);
+            }
+
+
             for (Path file : stream)
             {
                 if (Files.isRegularFile(file))
@@ -328,11 +301,11 @@ public class SetDatabase
                     Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
                 }
             }
-        } finally
+
+            System.out.println("Copied files from: " + source + " → " + destination);
+        } catch (IOException e)
         {
-
+            throw new DatabaseException("Failed to copy files from " + source + " to " + destination);
         }
-        System.out.println("Copied files from: " + source + " → " + destination);
     }
-
 }
